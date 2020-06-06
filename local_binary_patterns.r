28e1f13542ba1bbd8c5d40a452aa6a42bdfc5a2b
Jalaj Arjav Vora <jalaj.vora@st.ovgu.de>
2020-06-06 15:26:58 +0200

    Update local_binary_patterns.r

--- local_binary_patterns.r
+++ local_binary_patterns.r
@@ -58,24 +58,26 @@ library(e1071)
 
 #Plotting Dataset to see how it looks like
 #plot(iris)
-# s<- sample(150,100)
-# col<- c("Petal.Length", "Petal.Width", "Species")
-# iris_train <- iris[s,col]
-# iris_test <- iris[-s,col]
-# 
-# #Building SVM-Linear Model
-# svmfit <- svm(Species~., data=iris_train, kernel="linear", cost=0.1, scale=FALSE)
-# print(svmfit)
-# plot(svmfit, iris_train[,col])
-# 
-# 
-# #Cross-Validation Using tune() function:
-# tuned <- tune(svm, Species~., data=iris_train, kernel="linear", ranges = list(cost=c(0.001, 0.01, 0.1, 1, 10, 100)))
-# summary(tuned)
-# 
-# 
-# #Predicting using Test set
-# p <- predict(svmfit, iris_test[,col], type="class")
-# plot(p)
-# table(p, iris_test[,3])
-# mean(p == iris_test[,3])
+s<- sample(150,100)
+#print(s)
+col<- c(covid_data[59])
+#print(col)
+train <- covid_data[s,col]
+test <- covid_data[-s,col]
+
+#Building SVM-Linear Model
+svmfit <- svm(V59~., data=train, cost=0.1, scale=FALSE)
+print(svmfit)
+plot(svmfit, train[,col])
+
+
+#Cross-Validation Using tune() function:
+tuned <- tune(svm, V59~., data=train, kernel="linear", ranges = list(cost=c(0.001, 0.01, 0.1, 1, 10, 100)))
+summary(tuned)
+
+
+#Predicting using Test set
+p <- predict(svmfit, test[,col], type="class")
+plot(p)
+table(p, test[,3])
+mean(p == test[,3])
